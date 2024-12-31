c5b628ed4ade3f7e7fad60c57caca43c8b01df10
Diego Verdan Coelho <diegoverdan@gmail.com>
2024-12-31 14:16:11 -0300

    ajustes na página principal com seus componentes e definição de telas e cores no tailwind.

---
+++ src/components/RightBar.tsx
@@ -0,0 +1,5 @@
+function RightBar() {
+  return <div className="">RightBar</div>;
+}
+
+export default RightBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,22 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xsm: "500px",
+        sm: "600px",
+        md: "690px",
+        lg: "988px",
+        xl: "1078px",
+        xxl: "1265px",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        textGray: "#71767B",
+        textGrayLight: "#E7E9EA",
+        borderGray: "#2F3336",
+        inputGray: "#202327",
+        iconBlue: "#1D9BF0",
+        iconGreen: "#00BA7C",
+        iconPink: "#F91880",
       },
     },
   },
