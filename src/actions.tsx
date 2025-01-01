21e278635889940fadb7c4c8ce9182cf6f1db416
Diego Verdan Coelho <diegoverdan@gmail.com>
2025-01-01 20:40:06 -0300

    add actions.tsx para fazer upload de arquivos para o imagekit.io.

---
+++ src/actions.tsx
@@ -0,0 +1,35 @@
+"use server";
+
+import ImageKit from "imagekit";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
+
+export const shareAction = async (formData: FormData) => {
+  const file = formData.get("file") as File;
+  //const desc = formData.get("desc") as string;
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  imagekit.upload(
+    {
+      file: buffer,
+      fileName: file.name,
+      folder: "/posts",
+      transformation: {
+        pre: "w-600",
+      },
+    },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
+    }
+  );
+};
--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { shareAction } from "@/actions";
+import React, { useState } from "react";
 import Image from "./Image";
 
 function Share() {
+  const [media, setMedia] = useState<File | null>(null);
+
+  function handleMediaChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files[0]) {
+      setMedia(e.target.files[0]);
+    }
+  }
+
+  const previewURL = media ? URL.createObjectURL(media) : null;
+
   return (
-    <div className="p-4 flex gap-4">
+    <form className="p-4 flex gap-4" action={shareAction}>
       {/* AVATAR */}
       <div className="relative w-10 h-10 rounded-full overflow-hidden">
         <Image path="general/avatar.png" alt="" w={100} h={100} tr={true} />
@@ -13,18 +27,28 @@ function Share() {
         <input
           className="bg-transparent outline-none placeholder:text-textGray text-xl"
           type="text"
+          name="desc"
           placeholder="What is happening?"
         />
         <div className="flex justify-between items-center gap-4 flex-wrap">
           {/* ICONS */}
           <div className="flex gap-4 flex-wrap">
-            <Image
-              path="icons/image.svg"
-              alt=""
-              w={20}
-              h={20}
-              className="cursor-pointer"
+            <input
+              className="hidden"
+              id="file"
+              name="file"
+              type="file"
+              onChange={handleMediaChange}
             />
+            <label htmlFor="file">
+              <Image
+                path="icons/image.svg"
+                alt=""
+                w={20}
+                h={20}
+                className="cursor-pointer"
+              />
+            </label>
             <Image
               path="icons/gif.svg"
               alt=""
@@ -68,7 +92,7 @@ function Share() {
           </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
